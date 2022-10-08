42d3428ccddfb0c54e105516cc8421102e5897ae
Krishi V Bhat <vb.krishi@gmail.com>
2022-10-08 16:10:01 +0530

    Adds "Add Server" btn, whose status changes are tracked by property bound p element and a timer that uses string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean;
+  timer: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.resetCountdown();
   }
 
+  ngOnInit(): void {}
+
+  resetCountdown() {
+    this.allowNewServer = false;
+    this.timer = 5;
+
+    setTimeout(() => {
+      this.allowNewServer = true;
+      clearInterval(myInterval);
+    }, 5000);
+
+    const myInterval = setInterval(() => {
+      this.timer--;
+    }, 1000);
+  }
+
+  onClick() {
+    console.log('add server clicked');
+    this.resetCountdown();
+  }
+
+  get isAddServerAllowed() {
+    return this.allowNewServer;
+  }
 }

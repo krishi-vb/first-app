e37c9a94db82cdfe7b0f71455f615b7c37d2e96a
Krishi V Bhat <vb.krishi@gmail.com>
2022-10-09 22:03:21 +0530

    assignment 3: practicing directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { Component, Output } from '@angular/core';
 export class AppComponent {
   title = 'first-app';
   username: string = '';
+  userDetails = { name: 'Krishi', role: 'Dev' };
+  _showDetails = false;
+  counter = 0;
+  clicksLogArray: unknown[] = [];
 
   @Output()
   toggleBtnStatus: boolean;
@@ -22,5 +26,27 @@ export class AppComponent {
 
   resetUsername() {
     this.username = '';
+  }
+
+  onRequestDetailsClick(event: Event) {
+    this.counter++;
+    this._showDetails = !this._showDetails;
+    console.log(event);
+    this.clicksLogArray.push({
+      data: `Click no.${
+        this.counter
+      } was made at ${new Date().toLocaleString()}`,
+      counter: `${this.counter}`,
+    });
+  }
+
+  get showDetails() {
+    return this._showDetails;
+  }
+
+  checkCounter(counter: number) {
+    if (counter > 4) {
+      return 'blue';
+    }
   }
 }
